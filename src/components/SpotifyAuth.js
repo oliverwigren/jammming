eaad061524b354c099d8105416817cd2de1c075d
Oliver <oliver.wigren@hotmail.com>
2024-05-17 09:00:51 +0200

    clean up

--- src/components/SpotifyAuth.js
+++ src/components/SpotifyAuth.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const SpotifyAuth = (props) => {
-
   useEffect(() => {
     // Check if the URL contains access token after redirection
     const urlParams = new URLSearchParams(window.location.hash.substr(1));
-    const token = urlParams.get('access_token');
+    const token = urlParams.get("access_token");
 
     if (token) {
       // Save the access token to a variable or state
@@ -14,23 +13,28 @@ const SpotifyAuth = (props) => {
   }, []);
 
   const generateAccessToken = () => {
-    const clientId = 'cb64326a23414ed7a9cfca9afb18f6d8'; // Replace with your Spotify Client ID
-    const redirectUri = 'http://localhost:3000/callback'; // Replace with your redirect URI
-    const scopes = ['user-read-private', 'user-read-email', 'playlist-modify-private', 'playlist-modify-public']; // Add the scopes you need
+    const clientId = "cb64326a23414ed7a9cfca9afb18f6d8"; // Replace with your Spotify Client ID
+    const redirectUri = "http://localhost:3000/callback"; // Replace with your redirect URI
+    const scopes = [
+      "user-read-private",
+      "user-read-email",
+      "playlist-modify-private",
+      "playlist-modify-public",
+    ]; // Add the scopes you need
 
     const queryParams = {
       client_id: clientId,
       redirect_uri: redirectUri,
-      response_type: 'token',
-      scope: scopes.join(' '),
+      response_type: "token",
+      scope: scopes.join(" "),
     };
 
     const queryString = Object.keys(queryParams)
       .map((key) => `${key}=${encodeURIComponent(queryParams[key])}`)
-      .join('&');
+      .join("&");
 
     const authUrl = `https://accounts.spotify.com/authorize?${queryString}`;
-    
+
     // Redirect the user to Spotify authorization page
     window.location.href = authUrl;
   };
